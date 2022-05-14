acd39967a52c2b873d0d2e7f5ee3aded64e1c28e
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2022-05-14 12:06:51 +0100

    more geometry

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import * as THREE from 'three';
 import SceneInit from '../lib/SceneInit';
+import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
+
 
 function App() {
 
@@ -9,11 +12,39 @@ function App() {
     test.initialize();
     test.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial({ wireframe: true });
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-
+    boxMesh.position.x = -1;
     test.scene.add(boxMesh);
+
+
+    const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 6, 16);
+    const cylinderMaterial = new THREE.MeshNormalMaterial({ wireframe: true});
+
+    const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+    cylinderMesh.position.x = 5;
+
+    test.scene.add(cylinderMesh);
+
+    const teaPotGeomtery = new TeapotGeometry(0.5, 8);
+    const teaPotMaterial = new THREE.MeshNormalMaterial({ wireframe: true })
+
+    const teaPotMesh = new THREE.Mesh(teaPotGeomtery, teaPotMaterial);
+
+    teaPotMesh.position.x = 1;
+    test.scene.add(teaPotMesh);
+
+
+    const roundedBoxGeo = new RoundedBoxGeometry(1, 1, 1, 4, 0.1);
+    const roundedBoxMaterial = new THREE.MeshNormalMaterial({ wireframe: true })
+
+    const roundedBoxMesh = new THREE.Mesh(roundedBoxGeo, roundedBoxMaterial);
+
+    roundedBoxMesh.position.x = 3;
+    test.scene.add(roundedBoxMesh);
+
+
 
   }, [])
   return (
